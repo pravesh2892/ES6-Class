2dfc7312e6f3b80f740438d06469611b90ec4394
pravesh meena <pravesh.meena2892@gmail.com>
2023-07-02 12:43:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
+
+
 class API {
-  #secure;
 
-  constructor(url, method = 'GET') {
-    this.url = url;
-    this.method = method;
-    this.#setSecure();
-  }
+    #secure
+    
+    constructor(url, method = 'GET') {
+    
+    this.url = url
+    
+    this.method = method
+    
+    this.#secure = url.startsWith("https")
+    
+    }
+    
+    isSecure() {
+    
+    return this.#secure
+    
+    }
+    
+    updateUrl(url) {
+    
+    this.url = url
+    
+    this.#secure = url.startsWith("https")
+    
+    }
+    
+    }
 
-  isSecure() {
-    return this.#secure;
-  }
 
-  updateUrl(url) {
-    this.url = url;
-    this.#setSecure();
-  }
 
-  #setSecure() {
-    this.#secure = this.url.startsWith('https');
-  }
-}
 
-module.exports = API;
-
-const s = new API('http://api.com/api/hello');
-console.log(s.isSecure()); // false
-
-s.updateUrl('https://api.com/api/hello');
-console.log(s.isSecure()); // true
-
-console.log(s.url); // https://api.com/api/hello
-
-console.log(s.method); // GET
-
-console.log(s.secure); // undefined (private field)
+//Do not remove this code
+module.exports = { API }
